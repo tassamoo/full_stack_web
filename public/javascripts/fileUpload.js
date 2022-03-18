70e98ee7be233545f2f57d3be0f6c4f31abde4d0
tassamoo <tassamu@outlook.com>
2022-03-18 08:38:49 +0700

    File Upload Database

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -4,4 +4,10 @@ FilePond.registerPlugin(
     FilePondPluginFileEncode,
   )
 
+FilePond.setOptions({
+    stylePanelAspectRatio: 150/100,
+    imageResizeTargetHeight: 150,
+    imageResizeTargetWidth: 100
+})
+
 FilePond.parse(document.body);
