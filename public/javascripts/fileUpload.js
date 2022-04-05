5b8c30a3e9881a6c1bff0446c4820e8c6902fcb0
tassamoo <tassamu@outlook.com>
2022-04-05 15:29:28 +0700

    finished project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,33 @@
-FilePond.registerPlugin(
+const rootStyles = window.getComputedStyle(document.documentElement)
+
+if (rootStyles.getPropertyValue('--book-cover-width-large') != null) {
+  ready()
+} else {
+  document.getElementById('main-css')
+    .addEventListener('load', ready)
+}
+
+
+function ready() {
+  const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+  const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+  const coverHeight = coverWidth / coverAspectRatio
+
+  FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
     FilePondPluginFileEncode,
   )
 
 FilePond.setOptions({
-    stylePanelAspectRatio: 150/100,
-    imageResizeTargetHeight: 150,
-    imageResizeTargetWidth: 100
+    stylePanelAspectRatio: 1 / coverAspectRatio,
+    imageResizeTargetHeight: coverHeight,
+    imageResizeTargetWidth: coverWidth
 })
 
-FilePond.parse(document.body);
+FilePond.parse(document.body)
+
+}
+
+FilePond.parse(document.body)
+
